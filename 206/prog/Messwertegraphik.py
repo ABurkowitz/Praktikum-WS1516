02e55e3f4268edd2480e4ef63535daf1f37a923f
Phillip Greve <phillip.greve@udo.edu>
2015-10-31 12:54:50 +0100

    Aufgabe 5a fertig 5b fertig messbeginn in den txt datein von minute 1 auf minute 0 gesetzt

--- 206/prog/Messwertegraphik.py
+++ 206/prog/Messwertegraphik.py
@@ -1,15 +1,26 @@
 import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
-
-t, T1 , T2 = np.genfromtxt('Temperatur_array.txt', unpack=True)
+t, T1 , T2 = np.genfromtxt('daten/Temperatur_array.txt', unpack=True)
 T1 = T1 + 273.15
 T2 = T2 + 273.15
 
+def f(t,A,B,C):
+ return A * t **2 + B * t + C
+def g(t,a,b,c):
+ return a * t **2 + b * t +c
+
 plt.plot(t, T1 , 'rx', label="Temperatur1")
 plt.plot(t, T2 , 'bx', label="Temperatur2")
+parameters1, pocv = curve_fit(f, t, T1)
+parameters2, pocv = curve_fit(g, t, T2)
+plt.plot(t, f(t, *parameters1), 'r-', label='Fit T1')
+plt.plot(t, f(t, *parameters2), 'b-', label='Fit T2')
 plt.xlabel(r'$Zeit$')
 plt.ylabel(r'$Tempratur()$')
 plt.tight_layout()
 plt.legend(loc='best')
 plt.savefig('Temperaturgraphik.pdf')
+
+np.savetxt('ausglkurve_T1.txt',parameters1, header="A,B,C")
+np.savetxt('ausglkurve_T2.txt',parameters1, header="a,b,c")
