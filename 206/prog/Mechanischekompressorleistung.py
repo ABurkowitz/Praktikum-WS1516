e059de1b132695f3636d657b962a7855e54a00fd
Phillip Greve <phillip.greve@udo.edu>
2015-11-02 19:10:14 +0100

    etwas versucht hat nicht geklappt und es kommen immer noch falsche werte raus

--- 206/prog/Mechanischekompressorleistung.py
+++ 206/prog/Mechanischekompressorleistung.py
@@ -4,11 +4,13 @@ pa=pa+1   #Kg/(ms²)
 pb=pb+1   #Kg/(ms²)
 p=1       #Kg/(ms²)
 T0=273.15 #K
-k=1.14    #
+k=1.14
+k1=1/k    #
 p0=1      #Kg/(ms²)
 roh0=5.51 #g/l kg/m³
 dmdt =np.genfromtxt('daten/Massendurchsatz.txt', unpack=True)
-#t, T1 , T2 = np.genfromtxt('daten/Temperatur_array.txt',float, unpack=True)
+#Nmech1=1/(k-1)(pb*((pa/pb)**(1/k)) -pa)/(pa*roh0*T0/(T1[5] *p0))*dmdt[0]
+t, T1 , T2 = np.genfromtxt('daten/Temperatur_array.txt',float, unpack=True)
 #ideales gasgesetz
 # pV=nRT
 # pV/T=RT
@@ -24,6 +26,7 @@ dmdt =np.genfromtxt('daten/Massendurchsatz.txt', unpack=True)
 
 
 #roh=np.array([0,0,0,0], float)
+#(pa[5]*roh0*T0/(T1[5] *p0))
 #
 #roh_0=(pa*roh0*T0/(T1[5] *p0))
 #roh_1=(pa*roh0*T0/(T1[10]*p0))
@@ -34,12 +37,12 @@ dmdt =np.genfromtxt('daten/Massendurchsatz.txt', unpack=True)
 #roh[2]=roh_2
 #roh[3]=roh_3
 #Nmech=np.array([0,0,0,0], float)
-#Nmech=1/(k-1)(pb*((pa/pb)**(1/k)) -pa)/roh*dmdt
+#Nmech=1/(k-1)(pb*((pa/pb)**k1) -pa)/roh*dmdt
 #Nmech1=1/(k-1)(pb*((pa/pb)**(1/k)) -pa)/(pa*roh0*T0/(T1[5] *p0))*dmdt[0]
 #Nmech2=1/(k-1)(pb*((pa/pb)**(1/k)) -pa)/(pa*roh0*T0/(T1[10]*p0))*dmdt[1]
 #Nmech3=1/(k-1)(pb*((pa/pb)**(1/k)) -pa)/(pa*roh0*T0/(T1[15]*p0))*dmdt[2]
 #Nmech4=1/(k-1)(pb*((pa/pb)**(1/k)) -pa)/(pa*roh0*T0/(T1[19]*p0))*dmdt[3]
-#print (Nmech1)
+#print (Nmech[0])
 #print (Nmech2)
 #print (Nmech3)
 #print (Nmech4)
--- 206/prog/rechnen.py
+++ 206/prog/rechnen.py
@@ -7,15 +7,16 @@ pa1=2.1+1
 pa2=2.1+1
 pa3=2.2+1
 pa4=2.2+1
-T1=17.6+273.15
-T2=9.0+273.15
-T3=1.6+273.15
-T4=-1.0+273.15
+T1=28.0+273.15
+T2=36.9+273.15
+T3=45.2+273.15
+T4=-50.7+273.15
 #pa=pa+1   #Kg/(ms²)
 #pb=pb+1   #Kg/(ms²)
 p=1       #Kg/(ms²)
 T0=273.15 #K
 k=1.14    #
+k1=1/k
 p0=1      #Kg/(ms²)
 roh0=5.51 #g/l kg/m³
 #druck
@@ -31,12 +32,12 @@ dmdt1=-1.394630925991996971e-02
 dmdt2=-1.318260986553914521e-02
 dmdt3=-1.241891047115831551e-02
 dmdt4=-1.180795095565365452e-02
-Nmech1=1/(k-1)*(pb1*np.sqrt(1.14)[(pa1/pb1)]-pa1)/roh1
-Nmech2=1/(k-1)*(pb2*np.sqrt(1.14)[(pa2/pb2)]-pa2)/roh2
-Nmech3=1/(k-1)*(pb3*np.sqrt(1.14)[(pa3/pb3)]-pa3)/roh3
-Nmech4=1/(k-1)*(pb4*np.sqrt(1.14)[(pa4/pb4)]-pa4)/roh4
+Nmech1=1/(k-1)*(pb1*(pa1/pb1)**k1-pa1)/roh1*dmdt1
+Nmech2=1/(k-1)*(pb2*(pa2/pb2)**k1-pa2)/roh2*dmdt2
+Nmech3=1/(k-1)*(pb3*(pa3/pb3)**k1-pa3)/roh3*dmdt3
+Nmech4=1/(k-1)*(pb4*(pa4/pb4)**k1-pa4)/roh4*dmdt4
 
-print (Nmech1)
-print (Nmech2)
-print (Nmech3)
-print (Nmech4)
+print (roh1)
+print (roh2)
+print (roh3)
+print (roh4)
