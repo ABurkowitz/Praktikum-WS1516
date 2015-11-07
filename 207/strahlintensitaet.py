58392533d0294eb9e89927def9383dbd3e0eba9d
Annika Burkowitz <annika.burkowitz@tu-dortmund.de>
2015-11-07 11:31:33 +0100

    Auswertung und Diskussion fast fertig. Formeln fuer die Fehlerrechnung und Fitparameter des Plots der Strahlintensitaet in Abhaenigkeit des Abstands fehlen noch.

--- 207/strahlintensitaet.py
+++ 207/strahlintensitaet.py
@@ -1,22 +1,36 @@
 import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
+from uncertainties import correlated_values, correlation_matrix
 x,U = np.genfromtxt('messwerte_abstand.txt', unpack=True)
 
 
 def f(m,x,c):
  return m * x + c
 
+#def g(a,x,b):
+# return (a / x) + b
+#
+#def h(d,x,e):
+# return (d / x**2) + e
 
 parameters1, pcov = curve_fit(f, x, U)
+#parameters2, pcov2 = curve_fit(g, x, U)
+#parameters3, pcov3 = curve_fit(h, x, U)
 
-plt.plot(x, f(x, *parameters1), 'g-', label='Fit')
+plt.plot(x, f(x, *parameters1), 'g-', label='linearer Fit')
+#plt.plot(x, g(x, *parameters2), 'b-', label='1/r Fit')
+#plt.plot(x, h(x, *parameters3), 'y-', label='1/r^2 Fit')
 plt.plot(x, U, 'rx', label='Messwerte Thermospannung')
 plt.xlabel(r'$x / cm$')
 plt.ylabel(r'$U / mV$')
 plt.tight_layout()
 plt.legend(loc='best')
 plt.savefig('strahlintensitaet.pdf')
+parameters1 = correlated_values(parameters1, pcov)
+for param1 in parameters1:
+    print(param1)
+print()
 print(parameters1)
 print(pcov)
 #np.savetxt('daten/ausglkurve_pT.txt',parameters1, header="m,T,C")
--- 207/thermospannung.py
+++ 207/thermospannung.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
+from uncertainties import correlated_values, correlation_matrix
 T,Us,Ug,Uw,Um = np.genfromtxt('messwerte_temperatur.txt', unpack=True)
 
 # Mittelwert der Offsetspannung abziehen U0=0.0072
@@ -27,6 +28,7 @@ parameters1, pcov1 = curve_fit(f, T, Us)
 parameters2, pcov2 = curve_fit(f, T, Ug)
 parameters3, pcov3 = curve_fit(f, T, Uw)
 parameters4, pcov4 = curve_fit(f, T, Um)
+
 plt.plot(T, f(T, *parameters1), 'k-', label='Fit schwarz')
 plt.plot(T, f(T, *parameters2), 'g-', label='Fit glänzend')
 plt.plot(T, f(T, *parameters3), 'r-', label='Fit weiß')
@@ -38,18 +40,64 @@ plt.ylabel(r'$U / mV$')
 plt.tight_layout()
 plt.legend(loc='best')
 plt.savefig('thermospannung.pdf')
+
 print('Fit schwarz')
-print(parameters1)
-print(pcov1)
-print('Fit glänzend')
-print(parameters2)
-print(pcov2)
-print('Fit weiß')
-print(parameters3)
-print(pcov3)
+parameters1 = correlated_values(parameters1, pcov1)
+for param1 in parameters1:
+    print(param1)
+#epsilon_s == 1
+
+print('Fit glaenzend')
+parameters2 = correlated_values(parameters2, pcov2)
+for param2 in parameters2:
+    print(param2)
+epsilon_g = parameters2[0] / parameters1[0]
+print(epsilon_g)
+
+print('Fit weiss')
+parameters3 = correlated_values(parameters3, pcov3)
+for param3 in parameters3:
+    print(param3)
+epsilon_w = parameters3[0] / parameters1[0]
+print(epsilon_w)
+
 print('Fit matt')
-print(parameters4)
-print(pcov4)
+parameters4 = correlated_values(parameters4, pcov4)
+for param4 in parameters4:
+    print(param4)
+epsilon_m = parameters4[0] / parameters1[0]
+print(epsilon_m)
+
+# Aktuelle Ausgabe
+#Fit schwarz
+#(1.047+/-0.007)e-10
+#-0.836+/-0.010
+#Fit glaenzend
+#(7.3+/-0.5)e-12
+#-0.062+/-0.006
+#0.069+/-0.004
+#Fit weiss
+#(9.95+/-0.06)e-11
+#-0.794+/-0.008
+#0.951+/-0.009
+#Fit matt
+#(1.67+/-0.07)e-11
+#-0.131+/-0.009
+#0.159+/-0.006
+
+
+#print('Fit schwarz')
+#print(parameters1)
+#print(pcov1)
+#print('Fit glänzend')
+#print(parameters2)
+#print(pcov2)
+#print('Fit weiß')
+#print(parameters3)
+#print(pcov3)
+#print('Fit matt')
+#print(parameters4)
+#print(pcov4)
 #Fit schwarz
 #[  1.04688488e-10  -8.36232527e-01]
 #[[  5.57523512e-25  -7.42087980e-15]
