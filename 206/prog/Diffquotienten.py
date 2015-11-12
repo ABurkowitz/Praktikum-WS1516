998f7245cb34764174f18cc797b5e5af801e6326
Phillip Greve <phillip.greve@udo.edu>
2015-11-12 18:21:42 +0100

    Bilder entfernt

--- 206/prog/Diffquotienten.py
+++ 206/prog/Diffquotienten.py
@@ -1,14 +1,13 @@
 import numpy as np
+import sympy
 Paras1= np.genfromtxt('daten/ausglkurve_T1.txt', float, unpack=True)
 Paras2= np.genfromtxt('daten/ausglkurve_T2.txt', float, unpack=True)
-A=Paras1[0]
+A=ufloatParas1[0]
 B=Paras1[1]
 C=Paras1[2]
 a=Paras2[0]
 b=Paras2[1]
 c=Paras2[2]
-
-
 
 T1diffq = np.array([5 ,10 ,15 ,19],float)
 T2diffq = np.array([5 ,10 ,15 ,19],float)
@@ -16,7 +15,6 @@ T2diffq = np.array([5 ,10 ,15 ,19],float)
 #t von min nach sekunden
 T1diffq=2*A *T1diffq*60 +B
 T2diffq=2*a *T2diffq*60 +b
-
 
 np.savetxt( 'daten/Diffquo.txt' ,(T1diffq ,T2diffq),
 header="diffquotienten fuer t (5,10,15,19) T1 (28.0 ,36.9 ,45.2 ,50.7) T2 (21.8 ,17.6 ,9.0 ,1.6 ,-1.0)")
