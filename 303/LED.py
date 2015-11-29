a9349cdd507dae5993df5943e9307291a0e2886e
Phillip Greve <phillip.greve@udo.edu>
2015-11-29 15:00:04 +0100

    Bilder für Theorie302 eingefügt und theorie teilweise fertig

--- 303/LED.py
+++ 303/LED.py
@@ -7,17 +7,19 @@ import uncertainties.unumpy as unp
 from uncertainties import correlated_values, correlation_matrix
 from uncertainties import ufloat
 import math
-x = np.linspace(5.1, 180)
+x = np.linspace(5.1, 180,1000)
 g , d, v = np.genfromtxt('daten/LED_abstand.txt', unpack=True)
 v=v/g
-r=x*x
+r=d*d
 o=4*np.pi
-C=0.5
-def f(o,r):
- return  o/r
-plt.plot(d, v , 'rx', label="Messdaten")
 
-plt.plot(x,f(o,r), 'r-',label="Theoriekurve")
+
+def f(o,r):
+ return  1/(o*r**2)
+plt.plot(d, v , 'rx', label="Messdaten")
+#plt.xscale('log',basex=2)
+#plt.yscale('log',basey=2)
+plt.plot(d,f(o,r), 'r-',label="Theoriekurve")
 plt.xlabel(r'$Abstand / cm$')
 plt.ylabel(r'$Spannung / V$')
 plt.tight_layout()
