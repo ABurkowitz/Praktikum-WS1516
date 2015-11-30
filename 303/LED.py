c6992b38b73ce5df50580b2c1355d3853d03798a
Annika Burkowitz <annika.burkowitz@tu-dortmund.de>
2015-11-30 08:17:11 +0100

    doppeltlogarythmischer Fit bei der Abstandsmessung

--- 303/LED.py
+++ 303/LED.py
@@ -1,25 +1,27 @@
 import numpy as np
-import scipy.optimize
-from scipy.optimize import curve_fit
+#import scipy.optimize
+#from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
-import uncertainties as unc
-import uncertainties.unumpy as unp
-from uncertainties import correlated_values, correlation_matrix
-from uncertainties import ufloat
-import math
-x = np.linspace(5.1, 180,1000)
+#import uncertainties as unc
+#import uncertainties.unumpy as unp
+#from uncertainties import correlated_values, correlation_matrix
+#from uncertainties import ufloat
+#import math
 g , d, v = np.genfromtxt('daten/LED_abstand.txt', unpack=True)
 v=v/g
 r=d*d
 o=4*np.pi
 
 
-def f(o,r):
- return  1/(o*r**2)
+def f(x):
+ return  150/(4*np.pi*(x**2))
+
 plt.plot(d, v , 'rx', label="Messdaten")
-#plt.xscale('log',basex=2)
-#plt.yscale('log',basey=2)
-plt.plot(d,f(o,r), 'r-',label="Theoriekurve")
+plt.xscale('log')
+plt.yscale('log')
+plt.xlim(5.1, 150)
+x = np.linspace(5.1, 150,1000)
+plt.plot(x,f(x), 'r-',label="Theoriekurve")
 plt.xlabel(r'$Abstand / cm$')
 plt.ylabel(r'$Spannung / V$')
 plt.tight_layout()
