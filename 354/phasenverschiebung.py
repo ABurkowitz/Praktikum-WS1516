d68309e7f715f44bb53616355c6313f1f63ce73d
Annika Burkowitz <annika.burkowitz@tu-dortmund.de>
2015-12-01 22:13:14 +0100

    Messwerte V354

---
+++ 354/phasenverschiebung.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+#a: Abstand der Nulldurchgaenge
+#f: Frequenz
+f, Uc, a = np.genfromtxt('messwerte_phasenverschiebung.txt', unpack=True)
+b=1/f
+def phi(a,b):
+    return (a/b)*2*np.pi
+
+plt.plot(f,phi(a,b),'rx')
+plt.savefig('phasenverschiebung.pdf')
